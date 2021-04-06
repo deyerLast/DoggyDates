94938b4bbcfd616f5a74f84572fc9a719e62bc60
deyerLast <meyer.david221@gmail.com>
2021-04-06 14:11:04 -0500

    Changed the project to Java for more control and easier documentation to read. Registration using Firebase completed.  Basics of Logging in complete.

---
+++ app/src/main/java/com/example/myapplication/User.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+public class User {
+    public String fullName, dogBreed, email;
+
+    public User(){
+
+    }
+
+    public User(String fullName, String dogBreed, String email){
+        this.fullName = fullName;
+        this.dogBreed = dogBreed;
+        this.email = email;
+    }
+}
